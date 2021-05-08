cd251e7be50cee60b9413b4137c92095d422da9f
codgician <15964984+codgician@users.noreply.github.com>
2021-05-08 11:58:04 +0800

    test: closure variant of calculator

--- tests/calculator.rs
+++ tests/calculator.rs
@@ -16,35 +16,37 @@ use naive_parsec::combinators::*;
 use naive_parsec::core::{lazy, Parsable, Parser};
 use naive_parsec::primitives::*;
 
+/// digit := '0' | '1' | ... | '9'
 fn digit<'f>() -> Parser<'f, char, CharStream<'f>> {
     satisfy(|&ch| ch.is_digit(10))
 }
 
+/// uint := digit { digit }
 fn uint<'f>() -> Parser<'f, String, CharStream<'f>> {
     digit().some().map(|v| v.iter().collect::<String>())
 }
 
+/// float := uint {'.' uint}
 fn float<'f>() -> Parser<'f, f64, CharStream<'f>> {
     uint()
         .and(char('.').and(lazy(uint)).optional())
         .map_result(|(s, r)| {
             let mut res = s;
-            match r {
-                Some((dot, frac)) => {
-                    res.push(dot);
-                    res.push_str(&frac[..])
-                }
-                _ => {}
-            };
+            if let Some((dot, frac)) = r {
+                res.push(dot);
+                res.push_str(&frac[..])
+            }
             res.parse::<f64>()
         })
         .trim()
 }
 
+/// factor := '(' expr ')' | float
 fn factor<'f>() -> Parser<'f, f64, CharStream<'f>> {
     mid(char('(').trim(), lazy(expr), char(')').trim()).or(float())
 }
 
+/// term := factor ('*'|'/') term | factor
 fn term<'f>() -> Parser<'f, f64, CharStream<'f>> {
     factor()
         .and(char('*').or(char('/').trim()))
@@ -53,6 +55,7 @@ fn term<'f>() -> Parser<'f, f64, CharStream<'f>> {
         .or(lazy(factor))
 }
 
+/// expr := term ('+'|'-') expr | term
 fn expr<'f>() -> Parser<'f, f64, CharStream<'f>> {
     term()
         .and(char('+').or(char('-')).trim())
@@ -61,42 +64,91 @@ fn expr<'f>() -> Parser<'f, f64, CharStream<'f>> {
         .or(lazy(term))
 }
 
+/// Another equivlent implementation using closures
+fn expr_<'s>() -> impl Parsable<Stream = CharStream<'s>, Result = f64> {
+    fix(|expr| {
+        // digit := '0' | '1' | ... | '9'
+        let digit = satisfy(|&ch| ch.is_digit(10));
+        // uint := digit { digit }
+        let uint = digit.some().map(|v| v.iter().collect::<String>());
+        // float := uint {'.' uint}
+        let float = uint
+            .clone()
+            .and(char('.').and(uint).optional())
+            .map_result(|(s, r)| {
+                let mut res = s;
+                if let Some((dot, frac)) = r {
+                    res.push(dot);
+                    res.push_str(&frac[..])
+                }
+                res.parse::<f64>()
+            })
+            .trim();
+        // factor := '(' expr ')' | float
+        let factor = mid(char('(').trim(), expr.clone(), char(')').trim()).or(float);
+        // term := factor ('*'|'/') term | factor
+        let term = fix(move |term| {
+            factor
+                .clone()
+                .and(char('*').or(char('/').trim()))
+                .and(term.clone())
+                .map(|((v1, op), v2)| if op == '*' { v1 * v2 } else { v1 / v2 })
+                .or(factor.clone())
+        });
+        // expr := term ('+'|'-') expr | term
+        term.clone()
+            .and(char('+').or(char('-')).trim())
+            .and(expr.clone())
+            .map(|((v1, op), v2)| if op == '+' { v1 + v2 } else { v1 - v2 })
+            .or(term.clone())
+    })
+}
+
+/// Helper function for testing calculator
+fn test_helper(input: &str, expected: Option<f64>, rem_str: &str, log_size: usize) {
+    let mut st1 = CharStream::new(input);
+    let mut st2 = st1.clone();
+
+    let (res1, logs1) = expr().exec(&mut st1);
+    let (res2, logs2) = expr_().exec(&mut st2);
+
+    // `expr` and `expr_` should be equivalent
+    assert_eq!(res1, res2);
+    assert_eq!(st1.as_str(), st2.as_str());
+    assert_eq!(logs1, logs2);
+
+    // Test if matches expected result
+    assert_eq!(expected, res1);
+    assert_eq!(rem_str, st1.as_str());
+    assert_eq!(log_size, logs1.len());
+}
+
 #[test]
 fn int_expr() {
-    let mut st = CharStream::new("2+4*(6+0)/1");
-    let (res, logs) = expr().exec(&mut st);
-
-    assert_eq!(Some((2 + 4 * (6 + 0) / 1) as f64), res);
-    assert_eq!("", st.as_str());
-    assert_eq!(0, logs.len());
+    test_helper("2+4*(6+0)/1", Some((2 + 4 * (6 + 0) / 1) as f64), "", 0);
 }
 
 #[test]
 fn int_expr_with_whitespace() {
-    let mut st = CharStream::new(" 2 + 4 * ( 6 + 0 ) / 1 ");
-    let (res, logs) = expr().exec(&mut st);
-
-    assert_eq!(Some((2 + 4 * (6 + 0) / 1) as f64), res);
-    assert_eq!("", st.as_str());
-    assert_eq!(0, logs.len());
+    test_helper(
+        " 2 + 4 * ( 6 + 0 ) / 1 ",
+        Some((2 + 4 * (6 + 0) / 1) as f64),
+        "",
+        0,
+    );
 }
 
 #[test]
 fn float_expr() {
-    let mut st = CharStream::new("1.9/(2.6+0.8)+1.7");
-    let (res, logs) = expr().exec(&mut st);
-
-    assert_eq!(Some(1.9 / (2.6 + 0.8) + 1.7), res);
-    assert_eq!("", st.as_str());
-    assert_eq!(0, logs.len());
+    test_helper("1.9/(2.6+0.8)+1.7", Some(1.9 / (2.6 + 0.8) + 1.7), "", 0);
 }
 
 #[test]
 fn float_expr_with_whitespace() {
-    let mut st = CharStream::new("1.9 / ( 2.6 + 0.8 ) + 1.7 ");
-    let (res, logs) = expr().exec(&mut st);
-
-    assert_eq!(Some(1.9 / (2.6 + 0.8) + 1.7), res);
-    assert_eq!("", st.as_str());
-    assert_eq!(0, logs.len());
+    test_helper(
+        "1.9 / ( 2.6 + 0.8 ) + 1.7 ",
+        Some(1.9 / (2.6 + 0.8) + 1.7),
+        "",
+        0,
+    );
 }
