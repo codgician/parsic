1c4a5b5b1161b474d1c9d47e7e0a2993809d0ba1
codgician <codgician@users.noreply.github.com>
2021-06-02 14:06:53 +0000

    deploy: 9e07efb76addcc38b98d934a88f473c8c3df2635

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["parsic"] = {"name":"","dirs":[{"name":"combinators","files":["alternative.rs","applicative.rs","error.rs","fix.rs","functor.rs","lazy.rs","mod.rs","monad.rs","replicative.rs","sequential.rs"]},{"name":"core","files":["logger.rs","mod.rs","ops.rs","parser.rs"]},{"name":"primitives","files":["combinators.rs","mod.rs","stream.rs"]}],"files":["lib.rs"]};
+sourcesIndex["parsic"] = {"name":"","dirs":[{"name":"combinators","files":["alternative.rs","applicative.rs","error.rs","fix.rs","functor.rs","mod.rs","monad.rs","replicative.rs","sequential.rs"]},{"name":"core","files":["logger.rs","mod.rs","ops.rs","parser.rs"]},{"name":"primitives","files":["combinators.rs","mod.rs","stream.rs"]}],"files":["lib.rs"]};
 createSourceSidebar();
